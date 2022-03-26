e82d56b6bbf6012fb44c5dde847b93dd5beade92
Fabio Roman <romanfabio@protonmail.com>
2022-03-26 11:13:58 +0100

    Add beginTransaction, commitTransaction, saveTransaction and rollbackTransaction

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
 import * as tedious from 'tedious';
 
+export enum ISOLATION_LEVEL {
+    NO_CHANGE = tedious.ISOLATION_LEVEL.NO_CHANGE,
+    READ_UNCOMMITTED = tedious.ISOLATION_LEVEL.READ_UNCOMMITTED,
+    READ_COMMITTED = tedious.ISOLATION_LEVEL.READ_COMMITTED,
+    REPEATABLE_READ = tedious.ISOLATION_LEVEL.REPEATABLE_READ,
+    SERIALIZABLE = tedious.ISOLATION_LEVEL.SERIALIZABLE,
+    SNAPSHOT = tedious.ISOLATION_LEVEL.SNAPSHOT
+};
+
 export class Connection {
-    private config : tedious.ConnectionConfig;
-    private connection : tedious.Connection;
+    private config: tedious.ConnectionConfig;
+    private connection: tedious.Connection;
 
     constructor(config: tedious.ConnectionConfig) {
         this.config = config;
-        
-        if(config.options === undefined)
+
+        if (config.options === undefined)
             config.options = {};
         config.options.rowCollectionOnRequestCompletion = true;
 
         this.connection = new tedious.Connection(config);
     }
 
-    public async connect() : Promise<void> {
+    public async connect(): Promise<void> {
         const self = this;
         return new Promise((resolve, reject) => {
             self.connection.connect((err) => {
-                if(err)
+                if (err)
                     reject(err);
                 else {
                     resolve();
@@ -27,7 +36,7 @@ export class Connection {
         });
     }
 
-    public async close() : Promise<void> {
+    public async close(): Promise<void> {
         const self = this;
         return new Promise((resolve, reject) => {
             self.connection.on('end', () => {
@@ -38,16 +47,68 @@ export class Connection {
         });
     }
 
-    public async reset() : Promise<void> {
+    public async reset(): Promise<void> {
         const self = this;
         return new Promise((resolve, reject) => {
             self.connection.reset((err) => {
-                if(err)
+                if (err)
                     reject(err);
                 else {
                     resolve();
                 }
             });
+        });
+    }
+
+    public async beginTransaction(name?: string, isolationLevel?: ISOLATION_LEVEL): Promise<void> {
+        const self = this;
+        return new Promise((resolve, reject) => {
+            self.connection.beginTransaction((err) => {
+                if (err)
+                    reject(err);
+                else {
+                    resolve();
+                }
+            }, name, isolationLevel as (tedious.ISOLATION_LEVEL | undefined))
+        });
+    }
+
+    public async commitTransaction(name?: string): Promise<void> {
+        const self = this;
+        return new Promise((resolve, reject) => {
+            (self.connection as any).commitTransaction((err : Error) => {
+                if (err)
+                    reject(err);
+                else {
+                    resolve();
+                }
+            }, name);
+        });
+    }
+
+    public async rollbackTransaction(name?: string) : Promise<void> {
+        const self = this;
+        return new Promise((resolve, reject) => {
+            (self.connection as any).rollbackTransaction((err : Error) => {
+                if (err)
+                    reject(err);
+                else {
+                    resolve();
+                }
+            }, name);
+        });
+    }
+
+    public async saveTransaction(name?: string) : Promise<void> {
+        const self = this;
+        return new Promise((resolve, reject) => {
+            (self.connection as any).saveTransaction((err : Error) => {
+                if (err)
+                    reject(err);
+                else {
+                    resolve();
+                }
+            }, name);
         });
     }
 }
--- test/connection.test.ts
+++ test/connection.test.ts
@@ -1,4 +1,4 @@
-import {Connection} from "../src/index";
+import {Connection, ISOLATION_LEVEL} from "../src/index";
 import config from "./config";
 
 test('the connection is created and then closed', async () => {
@@ -11,4 +11,30 @@ test('the connection is created and then closed', async () => {
 
 test('the connection fails with empty config', async () => {
     expect(() => new Connection({})).toThrow();
+});
+
+test('the connections begins, save and commits a transaction', async () => {
+    const conn = new Connection(config);
+
+    await conn.connect();
+
+    await conn.beginTransaction('test', ISOLATION_LEVEL.READ_UNCOMMITTED);
+
+    await conn.saveTransaction('test');
+
+    await conn.commitTransaction('test');
+
+    await conn.close();
+});
+
+test('the connections begins and rollbacks a transaction', async () => {
+    const conn = new Connection(config);
+
+    await conn.connect();
+
+    await conn.beginTransaction('', ISOLATION_LEVEL.READ_UNCOMMITTED);
+
+    await conn.rollbackTransaction('');
+    
+    await conn.close();
 });
